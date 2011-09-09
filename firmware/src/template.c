481b006a7b83d48a4abfd3bc3f7f8a0ddfc26f99
Etienne Le Sueur <elesueur@cse.unsw.edu.au>
2011-09-09 23:39:51 +1000

    double buffer example in template

--- firmware/src/template.c
+++ firmware/src/template.c
@@ -57,6 +57,8 @@
 #include <scandal/uart.h>
 #include <scandal/stdio.h>
 
+#include <string.h>
+
 #if defined(lpc11c14) || defined(lpc1768)
 #include <project/driver_config.h>
 #include <project/target_config.h>
@@ -159,11 +161,16 @@ void in_channel_0_handler(int32_t value, uint32_t src_time) {
 	UART_printf("in_channel_0_handler got called with value %d time at source %u\n\r", (int)value, (unsigned int)src_time);
 }
 
+#define BUFFER_SIZE 128
+
 /* This is your main function! You should have an infinite loop in here that
  * does all the important stuff your node was designed for */
 int main(void) {
-	int i = 0;
-	uint32_t value = 0xaa;
+	/* variables for double buffer example */
+	char buf_1[BUFFER_SIZE];
+	char buf_2[BUFFER_SIZE];
+	char *current_buf;
+	struct UART_buffer_descriptor buf_desc_1, buf_desc_2;
 
 	setup();
 
@@ -175,8 +182,8 @@ int main(void) {
 	sc_time_t test_in_timer = sc_get_timer(); /* Initialise the timer variable */
 
 	/* Set LEDs to known states */
-	red_led(1);
-	yellow_led(0);
+	red_led(0);
+	yellow_led(1);
 
 	scandal_delay(100); /* wait for the UART clocks to settle */
 
@@ -184,8 +191,12 @@ int main(void) {
 	UART_printf("Welcome to the template project! This is coming out over UART1\n\r");
 	UART_printf("The 2 debug LEDs should blink at a rate of 1HZ\n\r");
 	UART_printf("If you configure the in channel 0, I should print a message upon receipt of such a channel message\n\r");
+	UART_printf("This also shows an example of double buffer reading from the UART. Enter the text 'time' and press enter\n\r> ");
 
 	scandal_register_in_channel_handler(0, &in_channel_0_handler);
+
+	UART_init_double_buffer(&buf_desc_1, buf_1, BUFFER_SIZE,
+								&buf_desc_2, buf_2, BUFFER_SIZE);
 
 	/* This is the main loop, go for ever! */
 	while (1) {
@@ -194,15 +205,23 @@ int main(void) {
 		 * the number of errors and the version of scandal */
 		handle_scandal();
 
+		current_buf = UART_readline_double_buffer(&buf_desc_1, &buf_desc_2);
+
+		/* UART_readline_double_buffer will return a pointer to the current buffer. */
+		if (current_buf != NULL) {
+			if (strncmp("time", current_buf, 4) == 0) {
+				UART_printf("The time is: %d\r\n> ", (int)sc_get_timer());
+			}
+		}
+
 		/* Send a UART and CAN message and flash an LED every second */
 		if(sc_get_timer() >= one_sec_timer + 1000) {
 			/* Send the message */
-			UART_printf("This is the 1 second timer... %u\n\r", i++);
 
 			/* Send a channel message with a blerg value at low priority on channel 0 */
 			scandal_send_channel(TELEM_LOW, /* priority */
 									0,      /* channel num */
-									value   /* value */
+									0xaa   /* value */
 			);
 
 			/* Twiddle the LEDs */
@@ -218,10 +237,12 @@ int main(void) {
 		 * feature. Your function will get called when a new message comes in */
 		if(scandal_get_in_channel_rcvd_time(TEMPLATE_TEST_IN) > test_in_timer) {
 
-			value = scandal_get_in_channel_value(TEMPLATE_TEST_IN);
+			
 
 			UART_printf("I received a channel message in the main loop on in_channel 0, value %u at time %d\n\r", 
-				(unsigned int)value, (int)scandal_get_in_channel_rcvd_time(TEMPLATE_TEST_IN));
+				(unsigned int)scandal_get_in_channel_value(TEMPLATE_TEST_IN), 
+				(int)scandal_get_in_channel_rcvd_time(TEMPLATE_TEST_IN)
+			);
 
 			if(scandal_get_in_channel_value(TEMPLATE_TEST_IN) == 1) {
 				toggle_red_led();
